bfb08f4a1bec412f85860e37995a22e79ffcf698
Virendra Singh Solanki <72618659+theViren@users.noreply.github.com>
2021-08-30 20:50:42 +0530

    Add files via upload

---
+++ React-Practice---Greetings-with-Props-main/React-Practice---Greetings-with-Props-main/src/components/Welcome.js
@@ -0,0 +1,11 @@
+// write code for Welcome component here
+import React from "react";
+
+function Welcome(props) {
+    return <>
+        <h1>Hey {props.name}!</h1>
+        <h2>Welcome to Newton School.</h2>
+    </>
+}
+
+export default Welcome;
